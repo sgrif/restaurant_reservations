d0197fde22786ab165eaf2debbd180f2b671d061
Sean Griffin <sean.griffin@boomtime.com>
2012-11-03 16:08:52 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestaurantReservations::Application.config.secret_token = '1053446318e31c9d3997e7c292f6d4cc70ebc8d5b387e97188aff2c84d212c3a83f49fed4ebfbebbfa9029659b61167484b2db77419f14ae127a390805110d33'
